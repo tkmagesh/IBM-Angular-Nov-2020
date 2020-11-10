12275826de56fec99a597a7ad8e4d1e1e037d0f6
Magesh Kuppan <tkmagesh77@gmail.com>
2020-11-10 15:09:21 +0530

    Salary Calculator implementation

--- first-app/src/app/app.module.ts
+++ first-app/src/app/app.module.ts
@@ -3,13 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { GreeterComponent } from './greeter/greeter.component';
+import { SalaryCalculatorComponent } from './salaryCalculator/salaryCalculator.component';
 
 /* Decorator - associate meta data information with a type */
 @NgModule({
   declarations: [
     /* register the UI entities - Component, Directive, Pipe */
-    AppComponent
-    , GreeterComponent
+    AppComponent,
+    GreeterComponent,
+    SalaryCalculatorComponent
   ],
   imports: [
     /* register the other modules you want to use */
@@ -21,7 +23,8 @@ import { GreeterComponent } from './greeter/greeter.component';
   bootstrap: [
     /* top most components that you use in the index.html file */
     //AppComponent
-    GreeterComponent
+    GreeterComponent,
+    SalaryCalculatorComponent
   ]
 })
 export class AppModule {}
---
+++ first-app/src/app/salaryCalculator/salaryCalculator.component.ts
@@ -0,0 +1,16 @@
+import { Component } from "@angular/core";
+import { SalaryCalculatorModel } from './salaryCalculatorModel';
+
+@Component({
+    selector : 'app-salary-calculator',
+    templateUrl: './salaryCalculator.component.html',
+    styleUrls: ['./salaryCalculator.component.css']
+})
+export class SalaryCalculatorComponent{
+    
+    model : SalaryCalculatorModel = new SalaryCalculatorModel();
+
+    /* onBasicChange(value){
+        this.model.basic = value;
+    } */
+}
