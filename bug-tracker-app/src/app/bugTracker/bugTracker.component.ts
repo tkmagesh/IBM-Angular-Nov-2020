4a5e10441c358560bd89de4124c75029791f9a58
Magesh Kuppan <tkmagesh77@gmail.com>
2020-11-11 12:34:33 +0530

    Bug as a model (using interface)

--- bug-tracker-app/src/app/bugTracker/bugTracker.component.ts
+++ bug-tracker-app/src/app/bugTracker/bugTracker.component.ts
@@ -1,9 +1,22 @@
 import { Component } from "@angular/core";
+import { Bug } from './models/Bug'
 
 @Component({
     selector : 'app-bug-tracker',
     templateUrl: './bugTracker.component.html'
 })
 export class BugTrackerComponent{
+    private newBugId : number = 0;
+    bugList : Bug[] = [];
 
+    onAddNewClick(newBugName : string){
+        const newBug : Bug = {
+            id : ++this.newBugId,
+            name : newBugName,
+            isClosed : false,
+            createdAt : new Date()
+        };
+
+        this.bugList.push(newBug);
+    }
 }
